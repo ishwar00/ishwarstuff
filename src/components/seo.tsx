f2ae8a296e633413d2f217d97e2494d4883875bd
Ishwar <ishwargowda100@gmail.com>
2025-01-14 11:50:19 +0530

    chore: remove siteMetadata from SEO component

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
 
 const Seo = ({ title }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <title>{title} | {data.site.siteMetadata.title}</title>
-  )
+  return <title>{title}</title>
 }
 
 export default Seo
